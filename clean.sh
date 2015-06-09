ede3a00f3363c2c02fd16e19e79226740c95b275
Jorge Dias <jorge.dias@xing.com>
2015-06-09 10:33:30 +0200

    Fix clean script

--- clean.sh
+++ clean.sh
@@ -1,3 +1,6 @@
+export DOCKER_HOST=tcp://172.20.20.10:12375
+unset DOCKER_TLS_VERIFY
+
 docker ps | grep haproxy | awk '{print $1}' | xargs docker rm -f
 docker ps | grep redis | awk '{print $1}' | xargs docker rm -f
 docker ps | grep sinatra | awk '{print $1}' | xargs docker rm -f
